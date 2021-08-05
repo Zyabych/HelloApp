4deb6b68fc8616f1e768c3f4830c3dc7d147111c
Maisuk Serg <zyaba1986@yandex.ru>
2021-08-05 21:42:07 +0300

    Add project files.

---
+++ HelloApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO
+            Console.Beep(523, 500);
+            Console.Beep(587, 500);
+            Console.Beep(659, 500);
+            Console.Beep(698, 500);
+            Console.Beep(784, 500);
+            Console.Beep(880, 500);
+            Console.Beep(988, 500);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.White;
+            float t = 25;
+            Console.WriteLine(t);
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+
+        }
+    }
+}
